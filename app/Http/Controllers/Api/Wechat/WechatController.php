3ba84d7c126cca82de67508371617e5e2be6b639
fengnan1 <811264657@qq.com>
2020-11-24 13:02:01 +0800

    'first'

---
+++ app/Http/Controllers/Api/Wechat/WechatController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\Wechat;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class WechatController extends Controller
+{
+    public  function index(){
+        return 'hello word';
+    }
+}
