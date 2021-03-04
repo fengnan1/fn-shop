6c4d2a95251cfb15b84eb9cf9611f4d7a3d5992d
fengnan1 <811264657@qq.com>
2021-03-04 15:28:45 +0800

    新建控制器和模型

---
+++ app/Http/Controllers/Admin/GoodsTypeController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\GoodsType;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class GoodsTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\GoodsType  $goodsType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(GoodsType $goodsType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\GoodsType  $goodsType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(GoodsType $goodsType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\GoodsType  $goodsType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, GoodsType $goodsType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\GoodsType  $goodsType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(GoodsType $goodsType)
+    {
+        //
+    }
+}
